2d86996a79e189ef0c676a68db385dea4e4373d1
Elvando J <elvandojhonny@gmail.com>
2026-06-12 12:57:14 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ use App\Models\Gedung;
 use App\Models\Fakultas;
 use App\Models\Peminjaman;
 use App\Models\Pengumuman;
+use App\Models\Setting;
 
 class DashboardController extends Controller
 {
@@ -40,10 +41,18 @@ class DashboardController extends Controller
             ->take(5)
             ->get();
 
+
         $pengumuman = Pengumuman::where('aktif', true)
             ->latest()
             ->take(3)
             ->get();
+
+        $jamOperasional = Setting::first()?->jam_operasional ?? '08:30 - 17:00';
+
+        $totalAktivitas = $aktivitas->count();
+
+        $statusSistem = !app()->isDownForMaintenance();
+
 
         return view('user.dashboard', compact(
             'totalPengajuan',
@@ -51,7 +60,10 @@ class DashboardController extends Controller
             'ditolak',
             'menunggu',
             'aktivitas',
-            'pengumuman'
+            'totalAktivitas',
+            'pengumuman',
+            'statusSistem',
+            'jamOperasional'
         ));
     }
 
@@ -60,19 +72,15 @@ class DashboardController extends Controller
     | DASHBOARD ADMIN
     |--------------------------------------------------------------------------
     */
-
-    public function admin()
+public function admin()
 {
     // SUPER ADMIN
     if (auth()->user()->role == 'superadmin') {
 
         $totalFakultas = Fakultas::count();
-
-        $totalGedung = Gedung::count();
-
-        $totalKelas = Kelas::count();
-
-        $totalUser = User::count();
+        $totalGedung   = Gedung::count();
+        $totalKelas    = Kelas::count();
+        $totalUser     = User::count();
 
         $aktivitas = Peminjaman::with(
                 'user',
@@ -82,10 +90,23 @@ class DashboardController extends Controller
             ->take(5)
             ->get();
 
-    }
+        $totalPeminjaman = Peminjaman::count();
 
-    // ADMIN FAKULTAS
-    else {
+        $totalDisetujui = Peminjaman::where(
+            'status',
+            'disetujui'
+        )->count();
+
+        $totalPending = Peminjaman::where(
+            'status',
+            'pending'
+        )->count();
+
+        $pengumuman = Pengumuman::latest()
+            ->take(5)
+            ->get();
+
+    } else {
 
         $fakultasId = auth()->user()->fakultas_id;
 
@@ -99,12 +120,7 @@ class DashboardController extends Controller
         $totalKelas = Kelas::whereHas(
             'gedung',
             function ($q) use ($fakultasId) {
-
-                $q->where(
-                    'fakultas_id',
-                    $fakultasId
-                );
-
+                $q->where('fakultas_id', $fakultasId);
             }
         )->count();
 
@@ -120,25 +136,69 @@ class DashboardController extends Controller
             ->whereHas(
                 'kelas.gedung',
                 function ($q) use ($fakultasId) {
-
-                    $q->where(
-                        'fakultas_id',
-                        $fakultasId
-                    );
-
+                    $q->where('fakultas_id', $fakultasId);
                 }
             )
             ->latest()
             ->take(5)
             ->get();
+
+        $totalPeminjaman = Peminjaman::whereHas(
+            'kelas.gedung',
+            function ($q) use ($fakultasId) {
+                $q->where('fakultas_id', $fakultasId);
+            }
+        )->count();
+
+        $totalDisetujui = Peminjaman::where(
+            'status',
+            'disetujui'
+        )
+        ->whereHas(
+            'kelas.gedung',
+            function ($q) use ($fakultasId) {
+                $q->where('fakultas_id', $fakultasId);
+            }
+        )
+        ->count();
+
+        $totalPending = Peminjaman::where(
+            'status',
+            'pending'
+        )
+        ->whereHas(
+            'kelas.gedung',
+            function ($q) use ($fakultasId) {
+                $q->where('fakultas_id', $fakultasId);
+            }
+        )
+        ->count();
+
+        $pengumuman = Pengumuman::latest()
+            ->take(5)
+            ->get();
+            
     }
+
+    $totalAktivitas = $aktivitas->count();
+
+    $statusSistem = !app()->isDownForMaintenance();
+
+    $jamOperasional = Setting::first()?->jam_operasional ?? '08:00 - 17:00';
 
     return view('admin.dashboard', compact(
         'totalFakultas',
         'totalGedung',
         'totalKelas',
         'totalUser',
-        'aktivitas'
+        'aktivitas',
+        'totalAktivitas',
+        'totalPeminjaman',
+        'totalDisetujui',
+        'totalPending',
+        'pengumuman',
+        'statusSistem',
+        'jamOperasional'
     ));
 }
 }
